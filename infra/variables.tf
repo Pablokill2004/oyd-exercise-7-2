2795381b273183b8c3a63d79b72de3a87799f9b0
Pablokill2004 <22006477@galileo.edu>
2026-06-07 22:17:46 -0600

    queues

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,15 +1,14 @@
+#Changer regions
 variable "aws_region" {
   description = "AWS region"
   type        = string
   default     = "us-east-1"
 }
 
-
 variable "queue_name" {
   description = "Name of the SQS queue"
   type        = string
 }
-
 
 variable "visibility_timeout_seconds" {
   description = "Visibility timeout in seconds"
